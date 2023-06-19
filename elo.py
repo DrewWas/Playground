331e583811aba32c22ee599b8e94c9b6063722bc
Andrew Wasielewski <awwasielewski@gmail.com>
2023-06-20 01:32:54 +0200

    elo

--- elo.py
+++ elo.py
@@ -5,12 +5,44 @@ R2 = int(input("Player B: "))
 
 
 
-def f(x,y):
+def probA(x,y):
     
     Ea = 1/(1 + 10 ** ((R2 - R1) / 400))
     Eb = 1/(1 + 10 ** ((R1 - R2) / 400))
 
-    return "Player A Rating : " + str(Ea) + "\n" + " Player B Rating: " + str(Eb)
+    print("Player A Win Probability : " + str(Ea) + "\n" +
+"Player B Win Probability : " + str(Eb))
+
+    return Ea
 
 
-print(f(R1, R2))
+def probB(x,y):
+    
+    Ea = 1/(1 + 10 ** ((R2 - R1) / 400))
+    Eb = 1/(1 + 10 ** ((R1 - R2) / 400))
+
+    print("Player A Win Probability : " + str(Ea) + "\n" +
+"Player B Win Probability : " + str(Eb))
+
+    return Eb
+
+
+
+
+def update_ranking(initial_score, expected_prob, outcome):
+    # Outcomes: Win - 1, Draw - 0.5, Loss - 0
+    K = 32
+    updated_score =  initial_score + K * (outcome -
+expected_prob)
+    
+    return updated_score
+
+
+print(probA(R1, R2))
+
+print(update_ranking(900, probA(R1, R2), 1))
+
+
+
+
+
