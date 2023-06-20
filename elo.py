7922ee16640200cd162c1a8ada09d08f14f2d5ae
Andrew Wasielewski <awwasielewski@gmail.com>
2023-06-20 02:50:03 +0200

    dope elo calculator

--- elo.py
+++ elo.py
@@ -1,8 +1,5 @@
 from tabulate import tabulate
 
-
-# Elo rating
-
 R1 = int(input("Player A: "))
 R2 = int(input("Player B: "))
 
@@ -16,11 +13,19 @@ def probB(x,y):
     Eb = 1/(1 + 10 ** ((R1 - R2) / 400))
     return Eb
 
-
+A_prob = probA(R1, R2)
+B_prob = probB(R1, R2)
 
 
 def update_ranking(initial_score, expected_prob, outcome):
     # Outcomes: Win - 1, Draw - 0.5, Loss - 0
+
+    """
+    This is the K-Factor Algorithm according to FIDE, it differs to USCF and Chess.com
+
+    For most players ( 700 <= ELO >= 1500 ), on Chess.com (For 10-min Games)                                the K-Factor is closer to K = 16
+    """
+
     if initial_score < 2400:
         K = 20
     else:
@@ -32,9 +37,7 @@ expected_prob)
 
 
 def dataFrame():
-    A_prob = probA(R1, R2)
-    B_prob = probB(R1, R2)
-    
+    #This could be cleaned 
     tabs = [["Player A Win Probability: ", A_prob, str(int(A_prob * 100)) + "%"],
             ["Player A Updated Elo for Win: ", round(update_ranking(R1, A_prob, 1))],
             ["Player A Updated Elo for Draw: ", round(update_ranking(R1, A_prob, 0.5))],
@@ -49,7 +52,4 @@ def dataFrame():
 
 
 dataFrame()
-
-
-
 
