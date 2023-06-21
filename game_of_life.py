9519e008b1f7ca6c957a21e3c9eac026ab3673bb
Andrew Wasielewski <awwasielewski@gmail.com>
2023-06-21 03:26:24 +0200

    were actually doin something

--- game_of_life.py
+++ game_of_life.py
@@ -9,13 +9,10 @@ pygame.display.set_caption("Game of Life")
 
 
 # GRID 
-GRID = [[i for i in range(25)] for j in range(25)]
-
+GRID = [[[i  * 32, False] for i in range(25)] for j in range(25)]
 
 
 # NEED TO GET POINTER (X,Y), IF CLICK DETECTED, SQUARE WITH THOSE COORDINATES ARE NOW COLORED
-
-# HIGHLIGHT BLOCK CURSER IS OVER 
 
 # NEED TO DO 1s/0s for GRID Linked List so that we can do evaluations for the actual game rules
 
@@ -23,24 +20,41 @@ GRID = [[i for i in range(25)] for j in range(25)]
 def draw_grid():
     for i in GRID:
         for j in i:
-
-
-            # fix this shit looking like its up and to the right 4 or 5 too many pixels
-            pygame.draw.rect(WIN, (0,138,255), pygame.Rect(225, 255, 32, 32))   # delete
-
-            pygame.draw.line(WIN, (255,255,255), (j * 32, 0), (j * 32, 800))
-            pygame.draw.line(WIN, (255,255,255), (0, j * 32), (800, j * 32))
+            pygame.draw.line(WIN, (255,255,255), (j[0], 0), (j[0], 800))
+            pygame.draw.line(WIN, (255,255,255), (0, j[0]), (800, j[0]))
 
 
 
 def update_blocks():
-    mouse_x,mouse_y = pygame.mouse.get_pos()
 
+    # False = Dead | True = Alive
+
+
+    # get positions
+    mouse_x,mouse_y = pygame.mouse.get_pos()
     x_pos = mouse_x - (mouse_x % 32)
     y_pos = mouse_y - (mouse_y % 32)
 
-    print((mouse_x,mouse_y), (x_pos, y_pos))
-    pygame.draw.rect(WIN, (0,138,255), pygame.Rect(x_pos, y_pos, 32, 32))
+    cell_status = GRID[y_pos // 32][x_pos // 32][1]
+
+    if cell_status:  
+        #alive --> dead
+        pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(x_pos, y_pos, 32, 32))
+        GRID[y_pos // 32][x_pos // 32][1] = False
+
+    else:
+        #dead --> alive
+        pygame.draw.rect(WIN, (0, 138, 255), pygame.Rect(x_pos, y_pos, 32, 32))
+        GRID[y_pos // 32][x_pos // 32][1] = True 
+
+
+    print(GRID, cell_status)
+
+
+
+
+
+# If block is already blue, and clicked again, it goes back to black
 
 
 
@@ -48,22 +62,25 @@ def main():
     run = True
 
 
+    #for i in GRID:  # delete
+     #   print(i)    # delete
+
+
     while run:
-        #mouse_x,mouse_y = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
 
-                #x_pos = mouse_x - (mouse_x % 32)
-                #y_pos = mouse_y - (mouse_y % 32)
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 update_blocks()
-                #print((mouse_x,mouse_y), (x_pos, y_pos))
-                #pygame.draw.rect(WIN, (0,138,255), pygame.Rect(x_pos, y_pos, 32, 32))
 
         draw_grid()
-        
         pygame.display.update()
 
 
 main()
+
+
+
+
+
