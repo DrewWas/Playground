3624e6b0faf7934c2d8e4c42b25f96b2b01c1abb
Andrew Wasielewski <awwasielewski@gmail.com>
2023-06-21 04:00:35 +0200

    selected squares

--- game_of_life.py
+++ game_of_life.py
@@ -1,5 +1,14 @@
 # Conways game of life
 
+"""
+HOW TO PLAY:
+-----------------------------------------------------------------------------------
+* Use your mouse to select or deselect blocks you would like to start off as 'alive'
+    - Blue is alive, black is dead
+
+* To begin the simulation, press RETURN
+"""
+
 import pygame
 
 # Set up
@@ -10,6 +19,7 @@ pygame.display.set_caption("Game of Life")
 
 # GRID 
 GRID = [[[i  * 32, False] for i in range(25)] for j in range(25)]
+SELECTED_SQUARES = []
 
 
 
@@ -21,7 +31,7 @@ def draw_grid():
 
 
 
-def update_blocks():
+def setUp_blocks():
 
     # False = Dead | True = Alive
 
@@ -36,29 +46,58 @@ def update_blocks():
         #alive --> dead
         pygame.draw.rect(WIN, (0, 0, 0), pygame.Rect(x_pos, y_pos, 32, 32))
         GRID[y_pos // 32][x_pos // 32][1] = False
+        SELECTED_SQUARES.remove( (x_pos // 32, y_pos // 32) )
 
     else:
         #dead --> alive
         pygame.draw.rect(WIN, (0, 138, 255), pygame.Rect(x_pos, y_pos, 32, 32))
         GRID[y_pos // 32][x_pos // 32][1] = True 
+        SELECTED_SQUARES.append( (x_pos // 32, y_pos // 32) )
+
+
+    print(SELECTED_SQUARES)
+
+    return SELECTED_SQUARES 
 
 
 
+# HOW DO WE ENSURE THAT BLOCKS CANNOT BE SELECTED AFTER THE EVOLVE FUNCTION HAS STARTED
+
+
+def get_neighbors():
+    return None
+
+
+
+def evolve():
+    print(GRID)
+    return None
+    """
+    RULES: 
+    * 
+
+    """
 
 
 
 
 def main():
     run = True
+    clock = pygame.time.Clock()
 
 
     while run:
         for event in pygame.event.get():
+            clock.tick(60)
             if event.type == pygame.QUIT:
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                update_blocks()
+                setUp_blocks()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print("START\n\n")  
 
         draw_grid()
         pygame.display.update()
