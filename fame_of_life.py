22e5970a361d171e87443197d96d5a9ce59621c4
Andrew Wasielewski <awwasielewski@gmail.com>
2023-07-02 16:19:31 -0400

    roadmap?

---
+++ fame_of_life.py
@@ -0,0 +1,14 @@
+# Create a NxN grid
+
+# Be able to select and deselct squares in that grid 
+
+# If the square is selected, its coordinates are added to an array
+
+# The game starts and a timestep increases once per unit time (n seconds)
+
+# For i in selected squares array, check rules. If a cell becomes alive, add it to
+# the array 
+
+# continue indefinetly 
+
+
