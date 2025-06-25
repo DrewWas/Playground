e6669064557dcb8ecaaed2974ec879f4cc2e2ac3
Andrew Wasielewski <awwasielewski@gmail.com>
2025-06-24 23:57:04 -0500

    inputs

--- verilog/matmul/matmul.sv
+++ verilog/matmul/matmul.sv
@@ -1,6 +1,20 @@
 
 
-module matmul #() ();
+module matmul #(
+    parameter N = 256;
+    parameter M = 256,
+    
+    parameter J = 256,
+    parameter K = 256
+) (
+
+    input logic clk,
+    input logic reset,
+    input logic [N-1:0] mat1 [M-1:0],
+    input logic [J-1:0] mat2 [K-1:0],
+    output logic [M-1:0] outmat [J:0]
+    
+);
 
 
 
@@ -9,4 +23,18 @@ endmodule
 
 
 
+
+// Make a new module that takes in two vectors and returns the dot product 
+
+// Use FFs to meet timing. Don't use an insane amount of logic 
+
+
+module dot_product(
+    input logic []
+
+);
+
+
+
+endmodule 
 
--- verilog/matmul/tb.sv
+++ verilog/matmul/tb.sv
@@ -2,6 +2,8 @@
 
 module tb;
 
+    // Test out multiple dimensions (square, tall, wide etc.)
+    // Change N,M,J,K to test non-square matrices
 
 
 endmodule 
